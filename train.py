631d2ce1df49a4bc62ab5a3072b6407b65645ab8
UltralyticsAssistant <web@ultralytics.com>
2026-05-11 03:22:01 +0000

    Auto-format by https://ultralytics.com/actions

--- train.py
+++ train.py
@@ -1,8 +1,9 @@
-import warnings, os
+import warnings
+
 # os.environ["CUDA_VISIBLE_DEVICES"]="-1"    # 代表用cpu训练 不推荐！没意义！ 而且有些模块不能在cpu上跑
 # os.environ["CUDA_VISIBLE_DEVICES"]="0"     # 代表用第一张卡进行训练  0：第一张卡 1：第二张卡
 # 多卡训练参考<YOLOV11配置文件.md>下方常见错误和解决方案
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 from ultralytics import YOLO
 
 # BILIBILI UP 魔傀面具
@@ -22,22 +23,23 @@ from ultralytics import YOLO
 
 # 想找到哪些yaml是做轻量化的话可以用get_all_yaml_param_and_flops.py脚本，这个脚本里面有对应的教程视频。
 
-if __name__ == '__main__':
-    model = YOLO('yolo11-C3k2-DSAN.yaml')    #337
+if __name__ == "__main__":
+    model = YOLO("yolo11-C3k2-DSAN.yaml")  # 337
     # model.load('yolo11n.pt') # loading pretrain weights
-    model.train(data=r'D:\qileo\leo code\ultralytics-yolo11-main\NEU-DET\data.yaml',
-                cache=False,
-                imgsz=640,
-                epochs=300,
-                batch=32,
-                close_mosaic=0, # 最后多少个epoch关闭mosaic数据增强，设置0代表全程开启mosaic训练
-                workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
-                # device='0,1', # 指定显卡和多卡训练参考<YOLOV11配置文件.md>下方常见错误和解决方案
-                optimizer='SGD', # using SGD
-                # patience=0, # set 0 to close earlystop.
-                # resume=True, # 断点续训,YOLO初始化时选择last.pt
-                # amp=False, # close amp | loss出现nan可以关闭amp
-                # fraction=0.2,
-                project='runs/train',
-                name='yolo11-C3k2-DSAN337',
-                )
+    model.train(
+        data=r"D:\qileo\leo code\ultralytics-yolo11-main\NEU-DET\data.yaml",
+        cache=False,
+        imgsz=640,
+        epochs=300,
+        batch=32,
+        close_mosaic=0,  # 最后多少个epoch关闭mosaic数据增强，设置0代表全程开启mosaic训练
+        workers=4,  # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
+        # device='0,1', # 指定显卡和多卡训练参考<YOLOV11配置文件.md>下方常见错误和解决方案
+        optimizer="SGD",  # using SGD
+        # patience=0, # set 0 to close earlystop.
+        # resume=True, # 断点续训,YOLO初始化时选择last.pt
+        # amp=False, # close amp | loss出现nan可以关闭amp
+        # fraction=0.2,
+        project="runs/train",
+        name="yolo11-C3k2-DSAN337",
+    )
